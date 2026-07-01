992e83157e23126c07bb5a47c251911bf9465ff7
Cevin Agustian <cevin.agust@gmail.com>
2026-07-01 20:47:15 +0700

    feat: add aktivasi portal

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,10 @@ const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
+        source: '/aktivasi',
+        destination: '/aktivasi.html'
+      },
+      {
         source: '/netflix-tool/:path*',
         destination: 'http://157.20.32.218:5006/:path*'
       }
